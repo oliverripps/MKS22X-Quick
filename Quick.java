fbc41dd3a355132b394ecf9d08fed2126d4fce0c
Oliver Ripps <oripps@stuy.edu>
2019-03-11 11:14:19 -0400

    partition without random

--- Quick.java
+++ Quick.java
@@ -2,13 +2,25 @@ public class Quick{
 
   public static int partition (int [] data, int start, int end){
     int l=4;//makerandom
-    int[] newdata = new int[];
-    for(int i=0;i<data.length;i++){
-      if()
+    int f=0;
+    int b=0;
+    int[] newdata = new int[end-start];
+    for(int i=start;i<end;i++){
+      if(i!=l){
+        if(data[i]>data[l]){
+          newdata[newdata.length-1-b];
+          b++;
+        }
+        if(data[i]<data[l]){
+          newdata[f];
+          f++;
+        }
+      }
     }
-
-
-
+    //f+1  and b-1 should be equal
+    newdata[f+1]=data[l];
+    data=newdata;
+    return f+1;
   }
   public static int check(int[] data, int index){
     int val=data[index];
