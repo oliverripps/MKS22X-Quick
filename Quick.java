ac66be07131a9caa0d63bd33bdb9d1d483cba037
Oliver Ripps <oripps@stuy.edu>
2019-03-13 10:57:20 -0400

    broke something

--- Quick.java
+++ Quick.java
@@ -1,24 +1,24 @@
 public class Quick{
 
   public static int partition (int [] data, int start, int end){
-    int l = (int)(Math.random() * (end - start)+start);
-    System.out.println(l);
-    int p=data[l];
+    int l = (int)(Math.random() * (end - start)+start);//pick random pivot
+    //System.out.println(l);
+    int p=data[l];//value at pivot
     data[l]=data[start];
-    data[start]=p;
+    data[start]=p;//swapped start and pivot
     l=start;
     start++;
-    while (start < end) {
-      if (data[start] > p) {
-        int n = data[start];
+    while (start < end) {//while there is still swappin to be done
+      if (data[start] > p) {//if startneeds to be swapped
+        int n = data[start];//swapping them to end
         data[start] = data[end];
         data[end] = n;
-        end --;
+        end --;//end is one less
       } else{
-        start ++;
+        start ++;//if in right position, go foraard
       }
     }
-    if (data[start] > p) {
+    if (data[start] > p) {//swapping back pivot
       data[l] = data[start - 1];
       data[start - 1] = p;
       l = start - 1;
@@ -31,7 +31,7 @@ public class Quick{
     return l;
   }
 
-  public static boolean check(int[] data, int index){
+  public static boolean check(int[] data, int index){//checks if partition works
     int val=data[index];
     for(int i=0;i<index;i++){
       if(data[i]==val){
@@ -47,18 +47,18 @@ public class Quick{
   }
 
   public static int quickselect(int[] data, int p) throws ArrayIndexOutOfBoundsException{
-    if (p >= data.length || p < 0){
+    if (p >= data.length || p < 0){//if not possible
       throw new ArrayIndexOutOfBoundsException();
     }
-    if(data.length==1){
-      return data[0];
+    if(data.length==1){//if only one element
+      return data[0];//return that element
     }
-    return quickselectH(data, p, 0, data.length - 1);
+    return quickselectH(data, p, 0, data.length - 1);//call helper
   }
 
   private static int quickselectH(int[] data, int p, int start, int end){
-    if (start>=end){
-      return data[p];
+    if (start>=end){//if they meet or overlap
+      return data[p];//youre done, base case
     }
     int pivot = partition(data, start, end);
     quickselectH(data,p,start,pivot-1);
